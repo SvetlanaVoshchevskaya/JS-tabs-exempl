5087bcd47706f030248a21d18bbfa4e55b78475d
Svetlana <VSveta.83@gmail.com>
2020-04-05 22:18:37 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
   const tab = document.querySelectorAll('.info-header-tab');
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', function() {
       tabContent[b].classList.add('show');
     }
   }
-  info.addEventListener('click', function(event) {
+  info.addEventListener('click', function (event) {
     let target = event.target;
     if (target && target.classList.contains('info-header-tab')) {
       for (let i = 0; i < tab.length; i++) {
@@ -29,4 +29,43 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     }
   });
+
+  // timer
+
+  const deadline = '2020-04-06';
+
+  function getTimeRemaning(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const second = Math.floor((t / 1000) % 60);
+    const min = Math.floor((t / 1000 / 60) % 60);
+    const hour = Math.floor(t / (1000 * 60 * 60));
+    return {
+      total: t,
+      second,
+      min,
+      hour,
+    };
+  }
+
+  function setClock(id, endtime) {
+    const timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds');
+    let timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaning(endtime);
+      hours.textContent = t.hour >= 10 ? t.hour : `0${t.hour}`;
+      minutes.textContent = t.min >= 10 ? t.min : `0${t.min}`;
+      seconds.textContent = t.second >= 10 ? t.second : `0${t.second}`;
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+      }
+    }
+  }
+  setClock('timer', deadline);
 });
