40f17f3101d44c73330a89a8a024489b82f46512
Svetlana <VSveta.83@gmail.com>
2020-04-20 18:21:52 +0300

    add script for forms

--- js/script.js
+++ js/script.js
@@ -5,19 +5,20 @@ window.addEventListener('DOMContentLoaded', function () {
   const info = document.querySelector('.info-header');
   const tabContent = document.querySelectorAll('.info-tabcontent');
 
-  function hideContent(a) {
+  const hideContent = (a) => {
     for (let i = a; i < tabContent.length; i++) {
       tabContent[i].classList.remove('show');
       tabContent[i].classList.add('hide');
     }
-  }
-  function showContent(b) {
+  };
+  hideContent(1);
+  const showContent = (b) => {
     if (tabContent[b].classList.contains('hide')) {
       tabContent[b].classList.remove('hide');
       tabContent[b].classList.add('show');
     }
-  }
-  info.addEventListener('click', function (event) {
+  };
+  info.addEventListener('click', (event) => {
     let target = event.target;
     if (target && target.classList.contains('info-header-tab')) {
       for (let i = 0; i < tab.length; i++) {
@@ -32,7 +33,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
   // timer
 
-  const deadline = '2020-04-06';
+  const deadline = '2020-04-07';
 
   function getTimeRemaning(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date());
@@ -53,12 +54,24 @@ window.addEventListener('DOMContentLoaded', function () {
       minutes = timer.querySelector('.minutes'),
       seconds = timer.querySelector('.seconds');
     let timeInterval = setInterval(updateClock, 1000);
+    function addZero(num) {
+      {
+        if (num <= 9) {
+          return `0${num}`;
+        } else {
+          return num;
+        }
+      }
+    }
 
     function updateClock() {
       let t = getTimeRemaning(endtime);
-      hours.textContent = t.hour >= 10 ? t.hour : `0${t.hour}`;
-      minutes.textContent = t.min >= 10 ? t.min : `0${t.min}`;
-      seconds.textContent = t.second >= 10 ? t.second : `0${t.second}`;
+      // hours.textContent = t.hour >= 10 ? t.hour : `0${t.hour}`;
+      // minutes.textContent = t.min >= 10 ? t.min : `0${t.min}`;
+      // seconds.textContent = t.second >= 10 ? t.second : `0${t.second}`;
+      hours.textContent = addZero(t.hour);
+      minutes.textContent = addZero(t.min);
+      seconds.textContent = addZero(t.second);
       if (t.total <= 0) {
         clearInterval(timeInterval);
         hours.textContent = '00';
@@ -68,4 +81,91 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   }
   setClock('timer', deadline);
+
+  // modal
+  const moreBtn = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    closeBtn = document.querySelector('.popup-close');
+  const descrBtn = document.querySelectorAll('.description-btn');
+
+  function openModal() {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  }
+  function closeModal() {
+    overlay.style.display = 'none';
+    document.body.style.overflow = '';
+  }
+
+  // function addMoreDescr() {
+  //   for (let i = 0; i < descrBtn.length; i++) {
+  //     descrBtn[i].addEventListener('click', openModal);
+  //   }
+  // }
+
+  // addMoreDescr();
+  moreBtn.addEventListener('click', openModal);
+  closeBtn.addEventListener('click', closeModal);
+  descrBtn.forEach((el) => el.addEventListener('click', openModal));
+
+  //Form
+  const message = {
+    loading: 'Loading ...',
+    success: 'Спасибо! Мы с Вами свяжемся!',
+    failure: ' Произошла ошибка связитесь с нами по телефону..',
+  };
+  const form = document.querySelector('.main-form');
+  const contactForm = document.querySelector('#form');
+  const input = form.getElementsByTagName('input');
+  const statusMessage = document.createElement('div');
+  statusMessage.classList.add('status');
+
+  function onSubmit(event) {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+    contactForm.appendChild(statusMessage);
+    //XMLHttpRequest
+    const req = new XMLHttpRequest();
+    req.open('POST', 'server.php');
+    req.setRequestHeader('Content-Type', 'application/json');
+
+    let formData = new FormData(form);
+    const obj = {};
+    formData.forEach((value, key) => (obj[key] = value));
+
+    const json = JSON.stringify(obj);
+    req.send(json);
+
+    // fetch
+
+    // await fetch('server.php', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify(obj),
+    // }).then((response) => {
+    //   if (response.ok) {
+    //     statusMessage.innerHTML = message.success;
+    //   } else {
+    //     statusMessage.innerHTML = message.failure;
+    //   }
+    // });
+
+    req.addEventListener('readystatechange', () => {
+      if (req.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+      } else if (req.readyState === 4 && req.status === 200) {
+        statusMessage.innerHTML = message.success;
+        setTimeout(closeModal, 2000);
+      } else {
+        statusMessage.innerHTML = message.failure;
+      }
+    });
+    for (let i = 0; i < input.length; i++) {
+      input[i].value = '';
+    }
+  }
+
+  form.addEventListener('submit', onSubmit);
+  contactForm.addEventListener('submit', onSubmit);
 });
